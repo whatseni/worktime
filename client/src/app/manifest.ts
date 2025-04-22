5a443d71c7df75381d433597207adeaa8df85c72
seeys <es2008e@naver.com>
2025-04-22 20:38:16 +0900

    0422 :: pwa icon 추가 및 readme 작성성

--- client/src/app/manifest.ts
+++ client/src/app/manifest.ts
@@ -2,12 +2,24 @@ import { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: 'App Test',
-    // short_name: 'PWA',
-    // description: 'A Progressvie Web App with Next.js',
-    // start_url: '/',
-    // display: 'standalone',
-    // background_color: '#ffffff',
-    // theme_color: '#000000',
+    name: '알바시간',
+    short_name: 'PartTime',
+    description: '근무 시간 등록하는 앱',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ]
   }
 }
